65ea01bfe60e2ccd03f3881702d043fd2a3fa39b
vishnu kumar <narayanavishnukumar@gmail.com>
2021-04-26 12:09:12 +0530

    pictures added

--- check.php
+++ check.php
@@ -1,27 +1,14 @@
 <!doctype html>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-Name:<input type="text" name="name" required /><br>
-Email:<input type="email" name="email" required /><br>
-Subject:<input type="text" name="subject" required /><br>
-Message:<input type="text" name="message" required /><br>
-<input type="submit" value="submit"/><br>
+<input type="text" name="username" required />
+<input type="password" name="password" required />
+<input type="submit" value="submit"/>
 </form>
 
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-ini_set("SMTP", "aspmx.l.google.com");
-ini_set("sendmail_from", "noreply@gmail.com");
-$to = "narayanavishnukumar@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com\r\n";
-
-
-if(mail($to,$subject,$txt,$headers)==true){
-echo "mail sent successfully";
-}
-else{
-echo "mail not sent";
-}
+$user=$_POST['username'];
+$userPassword=$_POST['password'];
+echo "Details:$user $userPassword";
 }
 ?>
