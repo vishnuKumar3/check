686a541127d007d3145c42f694fb67a9a33f7e4a
vishnu kumar <narayanavishnukumar@gmail.com>
2021-04-28 13:00:29 +0530

    logo added

--- exam/examentry.php
+++ exam/examentry.php
@@ -35,9 +35,9 @@ font-family:newfont;
 <div class="login"><h1>Login</h1></div>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 <h2>Username:
-<input type="text" id="username" name="user"  placeholder="username:" autofocus required style="width:300px;height:30px;margin-bottom:10px;"></h2>
+<input type="text" id='username' name="user"  placeholder="username:" autofocus required style="width:300px;height:30px;margin-bottom:10px;"></h2>
 <h2>Password:
-<input type="password" id="password" name="pass"  placeholder="password:" required style="width:300px;height:30px;margin-bottom:10px;">
+<input type="password" id='password' name="pass"  placeholder="password:" required style="width:300px;height:30px;margin-bottom:10px;">
 </h2>
 <input style="margin-left:5%;" type="submit" value="submit" onsubmit></input>
 </form>
@@ -79,7 +79,7 @@ $_SESSION['username1']=$user;
 //setcookie("Name",$user);
 //echo $_COOKIE["Name"];
 if(!empty($user) && !empty($pass)){
-	echo "<script>document.getElementById('username').value='';
+	echo "<script>alert('hii');document.getElementById('username').value='';
 		document.getElementById('password').value='';</script>";
 	$host="sql6.freemysqlhosting.net";
 	$username="sql6408612";
