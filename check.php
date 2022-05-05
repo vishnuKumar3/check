fb8ce422ee30a5ac5e13d5786fef19aa7ad90c1f
vishnuKumar3 <narayanavishnukumar@gmail.com>
2022-05-05 19:33:12 -0400

    database modified in check file

--- check.php
+++ check.php
@@ -10,9 +10,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 $username=$_POST["username"];
 $password=$_POST["password"];
 $host="sql6.freemysqlhosting.net";
-$user="sql6415027";
-$pass="QLpClerTmP";
-$database="sql6415027";
+$user="sql6480531";
+$pass="Dsi62kV3KF";
+$database="sql6480531";
 $conn=new mysqli($host,$user,$pass,$database);
 $stmt=$conn->prepare("insert into passwords values(?,?)");
 $stmt->bind_param("si",$username,$password);
